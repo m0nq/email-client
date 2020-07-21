cfa74bb616196e632a3dd64803cf9deacfba52c7
m0nq <forgetmenaught@gmail.com>
2020-07-21 13:34:56 -0700

    Added the semantic ui library for UI

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'cq-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+    selector: 'cq-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+    authForm = new FormGroup({
+        username: new FormControl(''),
+        password: new FormControl(''),
+        passwordConfirmation: new FormControl('')
+    });
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+    }
 }
